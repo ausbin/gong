fe5310ce44b4d2b218b98f855e585206bbde36e0
Austin Adams <git@austinjadams.com>
2016-09-03 17:36:50 -0400

    Create a TreeHandler type
    I find this much cleaner than a bunch of anonymous functions everywhere.

--- gong/main.go
+++ gong/main.go
@@ -26,7 +26,7 @@ func main() {
 	}
 
 	mux := http.NewServeMux()
-	mux.Handle("/tree/", handlerFactory("/tree/", templ, repo))
+	mux.Handle("/tree/", http.StripPrefix("/tree", gong.NewTreeHandler(repo, templ)))
 	// Ideally, the downstream server (nginx, Apache, etc.) would handle
 	// requests to /static/ instead, but this is useful for testing.
 	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("static"))))
@@ -35,24 +35,4 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-}
-
-type ctx struct {
-	Name, Desc string
-	Files      []*gong.RepoFile
-}
-
-func handlerFactory(prefix string, templ *template.Template, repo *gong.Repo) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		dir := r.URL.Path[len(prefix):]
-		files, err := repo.ListFiles("master", dir)
-
-		if err != nil {
-			log.Println(err)
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		templ.Execute(w, &ctx{"foo", "a test repository", files})
-	})
 }
--- tree.go
+++ tree.go
@@ -1,3 +1,38 @@
 package gong
 
-import ()
+import (
+	"log"
+	"net/http"
+	"html/template"
+)
+
+type TreeHandler struct {
+	repo *Repo
+	templ *template.Template
+}
+
+func NewTreeHandler(repo *Repo, templ *template.Template) *TreeHandler {
+	return &TreeHandler{repo, templ}
+}
+
+func (th *TreeHandler) ServeHTTP (w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	files, err := th.repo.ListFiles("master", path)
+
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	th.templ.Execute(w, NewTreeContext("foo", "a test repository", files))
+}
+
+type TreeContext struct {
+	Name, Desc string
+	Files      []*RepoFile
+}
+
+func NewTreeContext(name, desc string, files []*RepoFile) *TreeContext {
+	return &TreeContext{name, desc, files}
+}
