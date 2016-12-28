d325c02732e9b24e1ebd460ce6b7c58617e3754a
Austin Adams <git@austinjadams.com>
2016-12-27 22:20:38 -0500

    Add config file
    Read host/port to bind on, template dir, etc. from a config file instead
    of hardcoding them.

---
+++ config/config.go
@@ -0,0 +1,104 @@
+package config
+
+import (
+	"code.austinjadams.com/gong/models"
+	"gopkg.in/ini.v1"
+	"strings"
+)
+
+type Parser interface {
+	Global() *Global
+	Repos() []*models.Repo
+}
+
+func NewParser(path string) (Parser, error) {
+	p := &parser{}
+	err := p.parse(path)
+
+	if err != nil {
+		return nil, err
+	} else {
+		return p, nil
+	}
+}
+
+type Global struct {
+	// Bind address+port
+	Addr string
+	Port uint
+	// Server configuration
+	PathPrefix  string
+	TemplateDir string
+}
+
+type parser struct {
+	cfg    *ini.File
+	global *Global
+	repos  []*models.Repo
+}
+
+func (p *parser) parse(path string) error {
+	var err error
+	p.cfg, err = ini.Load(path)
+
+	if err != nil {
+		return err
+	}
+
+	if err = p.parseGlobal(); err != nil {
+		return err
+	}
+
+	if err = p.parseRepos(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *parser) parseGlobal() error {
+	// Default section (which will always exist) contains global settings
+	section, _ := p.cfg.GetSection("")
+
+	p.global = &Global{
+		Addr:        section.Key("addr").MustString("127.0.0.1"),
+		Port:        section.Key("port").MustUint(8050),
+		PathPrefix:  strings.TrimSuffix(section.Key("path_prefix").MustString("/"), "/"),
+		TemplateDir: section.Key("template_dir").MustString("templates"),
+	}
+
+	return nil
+}
+
+func (p *parser) parseRepos() error {
+	sections := p.cfg.Sections()
+
+	for _, section := range sections {
+		// Ignore the default section, which holds global settings
+		if section.Name() == ini.DEFAULT_SECTION {
+			continue
+		}
+
+		newRepo, err := models.NewRepo(
+			section.Name(),
+			section.Key("description").MustString(""),
+			section.Key("path").String(),
+		)
+
+		if err != nil {
+			return err
+		}
+
+		p.repos = append(p.repos, newRepo)
+	}
+
+	return nil
+}
+
+func (p *parser) Global() *Global {
+	return p.global
+}
+
+func (p *parser) Repos() []*models.Repo {
+	return p.repos
+}
--- gong/main.go
+++ gong/main.go
@@ -1,37 +1,45 @@
 package main
 
 import (
+	"code.austinjadams.com/gong/config"
 	"code.austinjadams.com/gong/handlers"
-	"code.austinjadams.com/gong/models"
 	"code.austinjadams.com/gong/templates"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 func main() {
 	if len(os.Args)-1 != 1 {
-		log.Fatalln("you must pass only the path to a git repository")
+		log.Fatalln("you must pass only the path to a configuration file")
 	}
 
-	templates, err := templates.NewLoader("templates")
+	cfg, err := config.NewParser(os.Args[1])
 
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	repo, err := models.NewRepo("gong", "a git repository viewer", os.Args[1])
+	templates, err := templates.NewLoader(cfg.Global().TemplateDir)
 
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	mux := http.NewServeMux()
-	mux.Handle("/tree/", http.StripPrefix("/tree", handlers.NewTreeHandler(repo, templates.Get("repo-tree"))))
 	// Ideally, the downstream server (nginx, Apache, etc.) would handle
 	// requests to /static/ instead, but this is useful for testing.
 	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("static"))))
-	err = http.ListenAndServe(":8050", mux)
+
+	// Register repository paths
+	for _, repo := range cfg.Repos() {
+		path := "/" + repo.Name + "/tree"
+		mux.Handle(path+"/", http.StripPrefix(path, handlers.NewTree(repo, templates.Get("repo-tree"))))
+	}
+
+	bindTo := cfg.Global().Addr + ":" + strconv.FormatUint(uint64(cfg.Global().Port), 10)
+	err = http.ListenAndServe(bindTo, mux)
 
 	if err != nil {
 		log.Fatalln(err)
