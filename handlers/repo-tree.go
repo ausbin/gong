65416b601dc31503ca76db2696ecb5a998fc957f
Austin Adams <git@austinjadams.com>
2016-12-29 21:30:53 -0500

    handlers/repo-tree: Don't hardcode repo tree path
    Use the URL reverser for the repo tree screen to extract the path to
    serve rather than hardcoding the length of the path to remove.

--- handlers/repo-tree.go
+++ handlers/repo-tree.go
@@ -20,8 +20,9 @@ func NewRepoTree(url url.Reverser, repo *models.Repo, templ *template.Template) 
 }
 
 func (rt *RepoTree) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// XXX Don't hardcode the path here
-	path := r.URL.Path[len("/"+rt.repo.Name+"/tree"):]
+	// The -1 leaves us with a leading slash in the resulting path — so an
+	// absolute path
+	path := r.URL.Path[len(rt.url.RepoTree(rt.repo.Name, "/", true))-1:]
 
 	// Redirect if path does not end in /
 	if path[len(path)-1] != '/' {
